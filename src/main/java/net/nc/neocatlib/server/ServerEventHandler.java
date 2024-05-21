d267963ea2bf0ebf6dd14537d4e1c4b051b02857
NC <keyetpro@gmail.com>
2024-05-21 19:11:56 +0300

    Update ServerEventHandler.java
    Thats was just a test

--- src/main/java/net/nc/neocatlib/server/ServerEventHandler.java
+++ src/main/java/net/nc/neocatlib/server/ServerEventHandler.java
@@ -15,14 +15,15 @@ public class ServerEventHandler {
     @Mod.EventBusSubscriber(modid = NeoCatLib.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.DEDICATED_SERVER)
     public class ForgeServerEventHandler
     {
-
+        /*
+        Usage example.
         @SubscribeEvent
         public static void onChatted(ServerChatEvent event)
         {
             NeoCatLibUtils.sendMessageToPlayer("Hello!", event.getPlayer());
             NeoCatLibUtils.sendGUIMessage("Hey", event.getPlayer());
         }
-
+        */
     }
 
     @Mod.EventBusSubscriber(modid = NeoCatLib.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.DEDICATED_SERVER)
